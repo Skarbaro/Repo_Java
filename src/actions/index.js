c247df31c10afba2f12f9beff97c0a32de4b463a
Alex Romaniuk <alaster.rom@gmail.com>
2019-12-03 18:31:49 +0200

    Revert "2"
    This reverts commit 493bf079141dd5eba9cf6561cbfb73a629aff6c7.

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export * from './user'
-export * from './todos'
-
-export const RESET_STATE = 'RESET_STATE';
-
-export const resetState = () => ({
-  type: RESET_STATE
-});
--- src/hocs/index.js
+++
@@ -1,6 +0,0 @@
-export { default as withAuthentication } from './withAuthentication'
-export { default as withAuthorization } from './withAuthorization'
-
-export { default as withUser } from './withUser'
-export { default as withTodos } from './withTodos'
-export { default as withTasks } from './withTasks'
